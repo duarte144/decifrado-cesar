98739ac829c786de9e1505e5f5c137822085d9f9
jose luis duarte pachon <160190997+duarte144@users.noreply.github.com>
2025-02-15 22:44:08 -0500

    Update simbolos.py

--- 1.3.py/simbolos.py
+++ 1.3.py/simbolos.py
@@ -10,7 +10,7 @@ def cifrar_mensaje(mensaje: str, clave: int):
     alfabeto = string.ascii_uppercase + string.ascii_lowercase + string.digits +  " "
     
     for letra in mensaje:
-        print(cifrar_letra(letra, clave, alfabeto), end="")  
+        print(cifrar_letra(letra, clave, abs), end="")  
     print()  
 
 def main():
